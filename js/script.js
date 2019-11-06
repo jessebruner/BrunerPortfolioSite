994d5a1eada093a5ca49fa40cf84831a9662c8e0
Jesse Bruner <jebruner1690@gmail.com>
2019-11-06 09:24:39 -0500

    First addition

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hamburger() {
+  var x = document.getElementById("nav");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+//adding a comment
