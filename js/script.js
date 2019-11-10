d16de9c0192e4960358088f73c583870e526c389
Jesse Bruner <jebruner1690@gmail.com>
2019-11-10 14:45:02 -0500

    Navigation

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function hamburger() {
-  var x = document.getElementById("nav");
-  if (x.style.display == "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-//adding a comment
